3a7f8fd24141dd15357516e42c687adc58d0ecb8
Samson Zhang <wwsamson@yahoo.com>
2020-09-20 23:51:26 -0400

    add tailwind, typescript

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, {Html, Head, Main, NextScript} from 'next/document';
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return {...initialProps}
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700;900&family=Rubik:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap"
+                        rel="stylesheet"/>
+                </Head>
+                <body>
+                <Main/>
+                <NextScript/>
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument;
